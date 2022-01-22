c42fb59fb05a7f78f8429ae8c2b04e906557e212
pramit-marattha <pramitmarattha5497@gmail.com>
2022-01-22 17:13:01 +0545

    Signup page update

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -8,12 +8,12 @@ const Signup = () => {
       <Header />
 
       <main className="flex-grow">
-        <section className="bg-gradient-to-b from-gray-100 to-white">
+        <section className="bg-gradient-to-b bg-purple-100">
           <div className="max-w-6xl mx-auto px-4 sm:px-6">
             <div className="pt-32 pb-12 md:pt-40 md:pb-20">
               {/* Page header */}
               <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-                <h1 className="h1">Welcome. To Aviyel's Newsletter Archives</h1>
+                <h1 className="h1">Subscribe to our Newsletter!</h1>
               </div>
 
               {/* Form */}
@@ -25,13 +25,14 @@ const Signup = () => {
                         className="block text-gray-800 text-sm font-medium mb-1"
                         htmlFor="email"
                       >
-                        Email <span className="text-red-600">*</span>
+                        Enter your email address{" "}
+                        <span className="text-red-600">*</span>
                       </label>
                       <input
                         id="email"
                         type="email"
-                        className="form-input w-full text-gray-800"
-                        placeholder="Enter your email address"
+                        className="form-input w-full text-black bg-purple-100"
+                        placeholder="Enter your email..."
                         required
                       />
                     </div>
@@ -39,34 +40,19 @@ const Signup = () => {
 
                   <div className="flex flex-wrap -mx-3 mt-6">
                     <div className="w-full px-3">
-                      <button className="btn text-white bg-blue-600 hover:bg-blue-700 w-full">
-                        Sign up for newsletter
+                      <button className="btn text-white bg-purple-600 hover:bg-purple-400 w-full">
+                        Sign up to our newsletter
                       </button>
                     </div>
                   </div>
                   <div className="text-sm text-gray-500 text-center mt-3">
-                    By signing up to our newsletter, you agree to the{" "}
+                    By signing up to our newsletter, <br />
+                    you agree to the{" "}
                     <a className="underline" href="#0">
                       terms & conditions
                     </a>
-                    , and our{" "}
-                    <a className="underline" href="#0">
-                      privacy policy
-                    </a>
-                    .
                   </div>
                 </form>
-                <div className="flex items-center my-6">
-                  <div
-                    className="border-t border-gray-300 flex-grow mr-3"
-                    aria-hidden="true"
-                  ></div>
-                  <div className="text-gray-600 italic"></div>
-                  <div
-                    className="border-t border-gray-300 flex-grow ml-3"
-                    aria-hidden="true"
-                  ></div>
-                </div>
               </div>
             </div>
           </div>
