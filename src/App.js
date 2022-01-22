d6146a3898092aa5616d2ac3ead1730f0fef54c8
pramit-marattha <pramitmarattha5497@gmail.com>
2022-01-22 11:12:27 +0545

    Modal added and app main update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
-import React from "react";
-import Signup from "./components/Signup";
+import React, { useEffect } from "react";
+import { Switch, Route, useLocation } from "react-router-dom";
+import AOS from "aos";
+import { focusHandling } from "cruip-js-toolkit";
+import "./assets/css/styles.scss";
+// components
+import Home from "./components/Home";
+import SignUp from "./components/Signup";
+
 function App() {
-  return <Signup />;
+  const location = useLocation();
+
+  useEffect(() => {
+    AOS.init({
+      once: true,
+      disable: "phone",
+      duration: 700,
+      easing: "ease-out-cubic",
+    });
+  });
+
+  useEffect(() => {
+    document.querySelector("html").style.scrollBehavior = "auto";
+    window.scroll({ top: 0 });
+    document.querySelector("html").style.scrollBehavior = "";
+    focusHandling("outline");
+  }, [location.pathname]); // triggered on route change
+
+  return (
+    <>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/signup">
+          <SignUp />
+        </Route>
+      </Switch>
+    </>
+  );
 }
 
 export default App;
