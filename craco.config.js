0136aa430790de3b33b048df723cccedc1ce1364
pramit-marattha <pramitmarattha5497@gmail.com>
2022-01-22 11:40:10 +0545

    Tailwind config path update

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,7 @@
 module.exports = {
   style: {
-    postcss: {
-      plugins: [
-        require("tailwindcss")("./src/assets/css/tailwind.config.js"),
-        require("autoprefixer"),
-      ],
+    postcssOptions: {
+      plugins: [require("tailwindcss"), require("autoprefixer")],
     },
   },
 };
