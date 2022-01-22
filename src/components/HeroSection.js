8b9b10ed08f530ce28606ea6c2a870fe775e66b6
pramit-marattha <pramitmarattha5497@gmail.com>
2022-01-22 14:17:44 +0545

    Header and Hero section update

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -73,7 +73,6 @@ function HerSection() {
             </div>
           </div>
 
-          {/* Hero image */}
           <div>
             <div
               className="relative flex justify-center mb-8"
@@ -83,77 +82,11 @@ function HerSection() {
               <div className="flex flex-col justify-center">
                 <img
                   className="mx-auto"
-                  src={require("../assets/images/Hero.png").default}
+                  src={require("../assets/images/Hero.png")}
                   width="768"
                   height="432"
-                  alt="Hero"
+                  alt="Newsletter Main Banner"
                 />
-                <svg
-                  className="absolute inset-0 max-w-full mx-auto md:max-w-none h-auto"
-                  width="768"
-                  height="432"
-                  viewBox="0 0 768 432"
-                  xmlns="http://www.w3.org/2000/svg"
-                  xmlnsXlink="http://www.w3.org/1999/xlink"
-                >
-                  <defs>
-                    <linearGradient
-                      x1="50%"
-                      y1="0%"
-                      x2="50%"
-                      y2="100%"
-                      id="hero-ill-a"
-                    >
-                      <stop stopColor="#FFF" offset="0%" />
-                      <stop stopColor="#EAEAEA" offset="77.402%" />
-                      <stop stopColor="#DFDFDF" offset="100%" />
-                    </linearGradient>
-                    <linearGradient
-                      x1="50%"
-                      y1="0%"
-                      x2="50%"
-                      y2="99.24%"
-                      id="hero-ill-b"
-                    >
-                      <stop stopColor="#FFF" offset="0%" />
-                      <stop stopColor="#EAEAEA" offset="48.57%" />
-                      <stop stopColor="#DFDFDF" stopOpacity="0" offset="100%" />
-                    </linearGradient>
-                    <radialGradient
-                      cx="21.152%"
-                      cy="86.063%"
-                      fx="21.152%"
-                      fy="86.063%"
-                      r="79.941%"
-                      id="hero-ill-e"
-                    >
-                      <stop stopColor="#4FD1C5" offset="0%" />
-                      <stop stopColor="#81E6D9" offset="25.871%" />
-                      <stop stopColor="#338CF5" offset="100%" />
-                    </radialGradient>
-                    <circle id="hero-ill-d" cx="384" cy="216" r="64" />
-                  </defs>
-                  <g fill="none" fillRule="evenodd">
-                    <circle
-                      fillOpacity=".04"
-                      fill="url(#hero-ill-a)"
-                      cx="384"
-                      cy="216"
-                      r="128"
-                    />
-                    <circle
-                      fillOpacity=".16"
-                      fill="url(#hero-ill-b)"
-                      cx="384"
-                      cy="216"
-                      r="96"
-                    />
-                    <g fillRule="nonzero">
-                      <use fill="#000" xlinkHref="#hero-ill-d" />
-                      <use fill="url(#hero-ill-e)" xlinkHref="#hero-ill-d" />
-                    </g>
-                  </g>
-                </svg>
               </div>
               <button
                 className="absolute top-full flex items-center transform -translate-y-1/2 bg-white rounded-full font-medium group p-4 shadow-lg"
@@ -172,7 +105,9 @@ function HerSection() {
                   <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10zm0 2C5.373 24 0 18.627 0 12S5.373 0 12 0s12 5.373 12 12-5.373 12-12 12z" />
                   <path d="M10 17l6-5-6-5z" />
                 </svg>
-                <span className="ml-3">Watch the full video (2 min)</span>
+                <span className="ml-3">
+                  Aviyel - Even ideas need propogation!
+                </span>
               </button>
             </div>
 
@@ -185,8 +120,10 @@ function HerSection() {
               <div className="relative pb-9/16">
                 <iframe
                   className="absolute w-full h-full"
-                  src="https://www.youtube.com/watch?v=ZXS-yZ6drq0&ab_channel=Aviyel"
+                  src="https://www.youtube.com/embed/ZXS-yZ6drq0"
                   title="Video"
+                  frameborder="0"
+                  allow="autoplay; encrypted-media"
                   allowFullScreen
                 ></iframe>
               </div>
