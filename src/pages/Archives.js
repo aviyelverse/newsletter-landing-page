aab14d5a3003c7babda9e8698b1d4123bc70500a
pramit-marattha <pramitmarattha5497@gmail.com>
2022-01-22 18:08:38 +0545

    Archives footer update

--- src/pages/Archives.js
+++ src/pages/Archives.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 function Archives() {
   return (
@@ -93,6 +94,7 @@ function Archives() {
           </div>
         </section>
       </main>
+      <Footer />
     </div>
   );
 }
